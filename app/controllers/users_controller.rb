b17da8f2ae30b85326b0782171fa36e75fdaa886
Karolis <tavosnukis@gmail.com>
2019-08-22 01:11:22 +0300

    Added mailer that sends email to new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             log_in @user
+            UserMailer.with(user: @user).welcome_email.deliver_now
             redirect_to root_url
         else
             render 'new'
